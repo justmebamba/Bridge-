e21b705730718e925ffb65cd747aac042c442100
Ashley Winston <ashmariewinston@gmail.com>
2025-12-03 11:59:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6068240579-e9698",
+  "appId": "1:263259542582:web:ccb98565137c4f07bff497",
+  "apiKey": "AIzaSyCmN-6p0WmwmYaqZhoonT8yOGqBdPtXd3M",
+  "authDomain": "studio-6068240579-e9698.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "263259542582"
+};
